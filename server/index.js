21d1f98b8d2511028b4de326ad8fb083ad448766
Charvi Gupta <charvigu231990@gmail.com>
2024-07-10 23:50:21 +0530

    server updated

--- server/index.js
+++ server/index.js
@@ -2,18 +2,23 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const mongoose = require('mongoose');
-require("dotenv").config({path: "server/config/.env"});
+require('dotenv').config({ path: './config/.env' });
+const mongoURI = "mongodb+srv://charvigu231990:"+process.env.mongo_password+"@cluster0.w3o6xgt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 mongoose
-.connect("mongodb+srv://charvigu231990:"+process.env.mongo_password+"@cluster0.jylbx2h.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-.then((x) => {
-    console.log("Connected to Mongo");
-})
-.catch((err) => {
-    console.log("Error connecting to Mongo");
-});
+  .connect(mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.error("Error connecting to MongoDB:", err);
+  });
+
 app.use(express.json());
 
 app.listen(port, () => {
-  console.log("Server is running on port "+port );
+  console.log("Server is running on port " + port);
 });
